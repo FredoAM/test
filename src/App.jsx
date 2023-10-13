a6b4428d6d933568950c0c9ccfa383b49732ad7a
Alfredo Aranda <fredoaranda@gmail.com>
2023-10-13 16:58:46 -0600

    new test

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import SignUp from './signUp';
+
 
 function App() {
-
-
-  const handleClick = async () => {
-    try {
-      const response = await fetch('https://ecommerce-fredo-shop-production.up.railway.app/prueba.php', {
-        method: 'GET',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-      const data = await response.json();
-      console.log(data)
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
 
 
 
 
   return (
     <>
-     <button onClick={handleClick}>Click me</button>
+     <SignUp/>
     </>
   )
 }
